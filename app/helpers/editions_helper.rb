950474ee772062b3dee9206f3b6725e2c69a5cde
Tanya Leshchenko <t.m.leshchenko@gmail.com>
2020-09-16 15:26:08 +0300

    add editions helper !!!

--- app/helpers/editions_helper.rb
+++ app/helpers/editions_helper.rb
@@ -1,2 +1,9 @@
 module EditionsHelper
+  def link_to_edition(work_id, edition)
+    link_to(edition.description, work_edition_path(work_id, edition.id))
+  end
+
+  def link_to_title(work_id, edition)
+    link_to(edition.title, work_edition_path(work_id, edition.id))
+  end
 end
